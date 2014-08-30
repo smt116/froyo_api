66d047184667364482699037e1a7c6630f2c0334
Maciej Malecki <smt116@gmail.com>
2014-08-30 20:23:04 +0200

    Added download method for photo of the day.

--- lib/froyo_api/photo.rb
+++ lib/froyo_api/photo.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module FroyoApi
   class Photo < Client
     attr_reader :href, :type
@@ -5,6 +7,36 @@ module FroyoApi
     def initialize
       @href = attributes['href']
       @type = attributes['type']
+    end
+
+    def filename
+      @filename ||= begin
+        uri = URI.parse(href)
+        File.basename(uri.path)
+      end
+    end
+
+    def download(directory = '.')
+      file = File.join(directory, filename)
+
+      File.open(file, 'wb') do |out|
+        process_response = lambda do |response|
+          response.read_body do |chunk|
+            out.write(chunk)
+          end
+        end
+
+        options = {
+          url:            href,
+          method:         :get,
+          block_response: process_response,
+          headers:        {
+            accept: type
+          }
+        }
+
+        RestClient::Request.execute(options)
+      end
     end
 
     private
--- spec/froyo_api/photo_spec.rb
+++ spec/froyo_api/photo_spec.rb
@@ -37,4 +37,39 @@ RSpec.describe FroyoApi::Photo do
   describe '.type' do
     it { expect(photo.type).to eq('image/jpeg') }
   end
+
+  describe '.filename' do
+    it { expect(photo.filename).to eq('test.jpg') }
+  end
+
+  describe '.download' do
+    before do
+      allow(RestClient::Request).
+      to receive(:execute).
+      with({
+        url:            'http://example.com/test.jpg',
+        method:         :get,
+        block_response: nil,
+        headers:        {
+          accept: 'image/jpeg'
+        }
+      })
+    end
+
+    context 'directory' do
+      context 'by default' do
+        it 'should eq ./test.jpg' do
+          expect(File).to receive(:open).with('./test.jpg', anything)
+          photo.download
+        end
+      end
+
+      context 'when directory argument is present' do
+        it 'should eq /tmp/test.jpg' do
+          expect(File).to receive(:open).with('/tmp/test.jpg', anything)
+          photo.download('/tmp')
+        end
+      end
+    end
+  end
 end
