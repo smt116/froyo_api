30cb582a73b5a0419314c121c2e26276897c577d
Maciej Malecki <smt116@gmail.com>
2014-08-30 21:44:52 +0200

    Removed ./lib from path in requires.

--- lib/froyo_api.rb
+++ lib/froyo_api.rb
@@ -1,8 +1,8 @@
 require 'froyo_api/version'
-require './lib/froyo_api/client'
-require './lib/froyo_api/password'
-require './lib/froyo_api/name'
-require './lib/froyo_api/photo'
+require 'froyo_api/client'
+require 'froyo_api/password'
+require 'froyo_api/name'
+require 'froyo_api/photo'
 
 module FroyoApi
   URL = 'http://api.froyo.io'
